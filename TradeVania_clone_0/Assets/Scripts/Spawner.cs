092ef9ab5366426d2b03ffca582c816e85cefbb4
Ross McLelland <rtm861@cs.bham.ac.uk>
2022-02-18 14:02:38 +0000

    Setting the correct rotational orientation of the spawned units

--- TradeVania_clone_0/Assets/Scripts/Spawner.cs
+++ TradeVania_clone_0/Assets/Scripts/Spawner.cs
@@ -13,6 +13,8 @@ public class Spawner : NetworkBehaviour
     // Set the distance from the main base that you can spawn a player
     [SerializeField] private float spawnDistance = 7.5f;
 
+    private Quaternion spawnRotation = Quaternion.Euler(0 , -90, 0);
+
     // Initialise main camera (for ray casting)
     private Camera mainCamera;
 
@@ -22,10 +24,13 @@ public class Spawner : NetworkBehaviour
     // Command on the server to spawn an attacker
     [Command] private void CmdSpawnAttacker(Vector3 Spawnpos)
     {
+        // Set the objects to initially face the correct direction
+        if (this.connectionToClient.connectionId != 0){spawnRotation = Quaternion.Euler(0, 90, 0);}     //As there are only 2 players you only need to check if the client isn't player one to decide the orientation of its attackers
+
         // Create the Attacker on the Server
         GameObject AttackerInstance = Instantiate(AttackerPrefab,           // The prefab for the attacker trying to be spawned in
                                                   Spawnpos,   // Get the position of the spawn location transform
-                                                  Quaternion.identity);  // Get the rotation
+                                                  spawnRotation);  // Get the rotation
 
         // Spawn the GameObject for all clients (make sure is in instantiatable prefabs)
         NetworkServer.Spawn(AttackerInstance,       // Use the above spawned attacker
@@ -49,25 +54,28 @@ public class Spawner : NetworkBehaviour
     [ClientCallback]       //Prevents the server from trying to run this method (as update runs on both client and server)
     private void Update()
     {
+        
+
         // Return if haven't clicked
         if(!Mouse.current.leftButton.wasPressedThisFrame) {return;}  // Return if you aren't clicking the right mouse button
         
+        // Only attempt to spawn if the player has authority
+        if(!hasAuthority){return;}
+
         // Figure out where we clicked
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());     // Creates a ray from camera to mouse position
 
         //Checks ray hits smth and gets the hit point
         if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)){return;}     // The raycast hit has a point variable which is a point in the world that the ray hit
 
-        // Set the maximum and minimum z values for spawning
-        float maxZ = this.transform.position.z + spawnDistance;
-        float minZ = this.transform.position.z - spawnDistance;
-
         // Only Spawn Units within a certain range of the players base
-        if(hit.point.z < minZ || hit.point.z > maxZ ) {Debug.Log("clicked outside allowed region");return;}
+        if(hit.point.z < this.transform.position.z - spawnDistance || 
+            hit.point.z > this.transform.position.z + spawnDistance) 
+            {return;}
 
         // Run the the server spawn function
-        CmdSpawnAttacker(hit.point);
-        
+        CmdSpawnAttacker(hit.point);   
     }
+
     #endregion
 }
